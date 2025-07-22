8ff031306c2dfc00a78bfbd6391edf9e0c815682
Lucas Paulo <lp152659@gmail.com>
2025-07-22 00:39:08 -0300

    chore: Configuração de cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+app.use(cors());
 
 //Banco de dados
 import { dbConection } from "./settings/db";
